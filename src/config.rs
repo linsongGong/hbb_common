e64cd824d9c58d08790a4163ac7ca53084055fb8
linsongGong <gonglinsong@gmail.com>
2025-09-02 08:02:53 +0000

    改为自己的 ID/ 中继服务器

--- src/config.rs
+++ src/config.rs
@@ -100,8 +100,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["192.168.208.28"];
+pub const RS_PUB_KEY: &str = "upQyQxdQJOpYMTGsfPxI1scfQOERC8OfshvGPsWJt3w=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
